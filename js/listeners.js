82b57dd488f166b5b1f7205eeff14d75042dfcb2
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-05-16 09:16:20 +0200

    fix evt

--- js/listeners.js
+++ js/listeners.js
@@ -1,29 +1,29 @@
 function onKeydown(evt) {
     let code = evt.code;
-    if (event.keyCode === 37 || code === 'ArrowLeft') {
+    if (evt.keyCode === 37 || code === 'ArrowLeft') {
         evt.inputStates.left = true;
-    } else if (event.keyCode === 38 || code === 'ArrowUp') {
+    } else if (evt.keyCode === 38 || code === 'ArrowUp') {
         evt.inputStates.up = true;
-    } else if (event.keyCode === 39 || code === 'ArrowRight') {
+    } else if (evt.keyCode === 39 || code === 'ArrowRight') {
         evt.inputStates.right = true;
-    } else if (event.keyCode === 40 || code === 'ArrowDown') {
+    } else if (evt.keyCode === 40 || code === 'ArrowDown') {
         evt.inputStates.down = true;
-    } else if (event.keyCode === 32 || code === 'Space') {
+    } else if (evtevent.keyCode === 32 || code === 'Space') {
         evt.inputStates.space = true;
     }
 }
 
 function onKeyup(evt) {
     let code = evt.code;
-    if (event.keyCode === 37 || code === 'ArrowLeft') {
+    if (evt.keyCode === 37 || code === 'ArrowLeft') {
         evt.inputStates.left = false;
-    } else if (event.keyCode === 38 || code === 'ArrowUp') {
+    } else if (evt.keyCode === 38 || code === 'ArrowUp') {
         evt.inputStates.up = false;
-    } else if (event.keyCode === 39 || code === 'ArrowRight') {
+    } else if (evt.keyCode === 39 || code === 'ArrowRight') {
         evt.inputStates.right = false;
-    } else if (event.keyCode === 40 || code === 'ArrowDown') {
+    } else if (evt.keyCode === 40 || code === 'ArrowDown') {
         evt.inputStates.down = false;
-    } else if (event.keyCode === 32 || code === 'Space') {
+    } else if (evt.keyCode === 32 || code === 'Space') {
         evt.inputStates.space = false;
     }
 }
